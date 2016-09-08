80e0e0f0ad016dc05fd09f0f651a18709fd136b8
Jeremy Thompson <fenwood@gmail.com>
2016-09-07 23:29:34 -0400

    add gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #post 'contact', to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'visitors#new'
 end
